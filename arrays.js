ba85470e1cc1eb2a97a7d59d7a8e0700a27eb94d
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-02 00:56:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,3 +15,41 @@ function destructivelyAddElementToBeginningOfArray(array, string)
   array.unshift(string)
   return array
 }
+function addElementToEndOfArray(array, string)
+{
+  var newArray = [...array, string]
+  return newArray
+}
+function destructivelyAddElementToEndOfArray(array, string)
+{
+  array.push(string)
+  return array
+}
+function accessElementInArray(array, index)
+{
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array)
+{
+  var newArray = array.slice(1)
+  return newArray
+}
+function destructivelyRemoveElementFromEndOfArray(array)
+{
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array)
+{
+  var newArray = array.slice(0, array.length - 1)
+  return newArray
+}
+
+
+
+
